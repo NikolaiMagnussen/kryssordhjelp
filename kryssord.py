22f25e8e1e11c8d5e00ce3a98fdf04bfb7bc8f7b
Nikolai Magnussen <nikolai@magnussen.tf>
2020-08-06 20:20:03 +0200

    Adds support for kryssordkjempen

--- kryssord.py
+++ kryssord.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import requests
+from bs4 import BeautifulSoup
 
 def gratiskryssord(word):
     return _gratiskryssord(word, 0, [])
+
 
 def _gratiskryssord(word, page, output):
     res = requests.get(f"https://gratiskryssord.no/api/crosswordbook/scroll/{word}/-1/{page}")
@@ -11,6 +13,17 @@ def _gratiskryssord(word, page, output):
         return _gratiskryssord(word, page+1, [*output, *data['data']])
     return output
 
+
+def kryssordkjempen(word):
+    res = requests.get(f"https://kryssordkjempen.no/synoapp/search.php?query={word}&pattern=")
+    soup = BeautifulSoup(res.text, 'html.parser')
+    results = soup.find("ul", "resultlist")
+    out = []
+    if "Søket ditt ga ingen treff" not in results.text:
+        for letter in results.find_all("li", "header"):
+            out.append({"strLength": letter.find("h2").text.split()[-2],
+                         "list": [synonym.find("a").text for synonym in letter.find_all("li", "synonym")]})
+    return out
 
 
 def main():
@@ -24,21 +37,42 @@ def main():
         print("Error: you must provide a word!")
         return
     res = gratiskryssord(word)
-    if res is None:
+    res2 = kryssordkjempen(word)
+    if res is None and res2 is None:
         print("We could not find that word :(")
         return
 
+    results = set()
+
     # Filter length
-    if length != "":
-        res_length = [x['list'] for x in res if x['strLength'] == str(length)]
+    if res is not None:
+        if length != "":
+            res_length = [x['list'] for x in res if x['strLength'] == str(length)]
+        else:
+            res_length = [x['list'] for x in res]
+        res_flattened = [y for x in res_length for y in x]
+
+        # Filter contains
+        res_final = [x for x in res_flattened if contains in x]
     else:
-        res_length = [x['list'] for x in res]
-    res_flattened = [y for x in res_length for y in x]
+        res_final = []
 
-    # Filter contains
-    res_final = [x for x in res_flattened if contains in x]
+    # Filter length
+    if res2 is not None:
+        if length != "":
+            res_length2 = [x['list'] for x in res2 if x['strLength'] == str(length)]
+        else:
+            res_length2 = [x['list'] for x in res2]
+        res_flattened2 = [y for x in res_length2 for y in x]
 
-    pprint(res_final, indent=4)
+        # Filter contains
+        res_final2 = [x for x in res_flattened2 if contains in x]
+    else:
+        res_final2 = []
+
+    # Print output
+    final = set(res_final + res_final2)
+    pprint(final, indent=4)
 
 
 if __name__ == "__main__":
